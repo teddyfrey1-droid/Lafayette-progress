a3ce6378e28054fce18b1eb7fab74ac0ee10bd2f
Lafayette-progress <teddy.frey1@gmail.com>
2026-01-19 14:18:21 +0100

    Delete lib directory

--- lib/use-current-user.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useAuth } from "@/components/auth/auth-provider";
-
-// Ce hook sert de pont pour récupérer l'utilisateur connecté partout dans l'app
-export function useCurrentUser() {
-  const { user } = useAuth();
-  return user;
-}
