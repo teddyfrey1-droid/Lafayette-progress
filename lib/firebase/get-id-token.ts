75bedcaabae7ca0c76fbecfd24682b67a9c2321d
Lafayette-progress <teddy.frey1@gmail.com>
2026-01-21 16:15:39 +0100

    Delete lib directory

--- lib/firebase/get-id-token.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/lib/firebase/client"
-
-export async function getAuthHeader(): Promise<Record<string, string>> {
-  const user = auth.currentUser
-  if (!user) return {}
-  const token = await user.getIdToken()
-  return { Authorization: `Bearer ${token}` }
-}
