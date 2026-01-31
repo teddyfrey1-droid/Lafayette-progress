6559b939063d2b98c6f8ba5fb069c54ba3411482
Lafayette-progress <teddy.frey1@gmail.com>
2026-01-31 19:25:20 +0100

    Delete lib directory

--- lib/demo/orders-store.ts
+++
@@ -1,7 +0,0 @@
-"use client"
-/**
- * Pont de compatibilité.
- * Les pages importent "@/lib/demo/orders-store".
- * L'implémentation unique du store se trouve dans "@/lib/lib/demo/orders-store".
- */
-export * from "../lib/demo/orders-store"
